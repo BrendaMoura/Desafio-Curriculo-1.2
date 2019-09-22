ae0dc7027da21164522125c892a583d6e08f30d9
Brendaaaaaa <48135546+Brendaaaaaa@users.noreply.github.com>
2019-09-22 17:44:04 -0400

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function clicou(x) {
-    var c = "computador";
-    var l = "livros";
-    var p = "poemas";
-    if (x == c) {
-        document.getElementById(x).style.backgroundColor = 'red';
-    }
-}
