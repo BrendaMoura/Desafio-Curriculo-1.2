a1b7ab5e70eccae25d53823c03214a8f266d76f8
Brendaaaaaa <48135546+Brendaaaaaa@users.noreply.github.com>
2019-09-22 17:38:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function clicou(x) {
+    var c = "computador";
+    var l = "livros";
+    var p = "poemas";
+    if (x == c) {
+        document.getElementById(x).style.backgroundColor = 'red';
+    }
+}
